3d1392599945bddb66781f612f1280182417aa85
gogenjack <ivajackdev@gmail.com>
2025-12-08 00:07:45 +0100

    Rust crash course

---
+++ examples/hello.rs
@@ -0,0 +1,9 @@
+// Attributes - meta data for the compiler
+#![allow(unused)]
+
+// main () is the entry point of Rust program
+fn main() {
+// Macros is Rust generte code at compile time and are 
+// invoked with an exclamation mark (!)
+    println!("Hello, world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// cargo init PROJECT_NAME
+// cargo build
+// cargo fmt
+// cargo test 
+
+fn main() {
+    println!("Hello Rust");
+}
