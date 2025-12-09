cd68be9bc5007f21367103441349b692e774ca91
gogenjack <ivajackdev@gmail.com>
2025-12-09 20:02:35 +0100

    Variables in Rust

---
+++ examples/var.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+
+fn main() {
+    // Variables in Rust
+    // - Immutable by default
+    // - Use mut keyword to make it mutable
+    // let's declare some variables
+    let x = 1;
+    x += 1; // This will cause a compile-time error
+
+    let y = -1;
+
+}
