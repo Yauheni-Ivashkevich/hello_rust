96117eca5fc5590158532d8a3f92cd69efba413a
gogenjack <ivajackdev@gmail.com>
2025-12-10 19:53:36 +0100

    Variable exercises

--- examples/var.rs
+++ examples/var.rs
@@ -1,13 +1,36 @@
 #![allow(unused)]
 
+ // Constants 
+    const NUM: u32 = 1;
+
 fn main() {
     // Variables in Rust
     // - Immutable by default
     // - Use mut keyword to make it mutable
     // let's declare some variables
+    let mut x = 1;
+    x += 1;
+
+    // Types inference
+    let y: i32 = -1;
+    let z = -1;
+
+    // Shadowing
+    let x: i32 = 1;
+    let x: i32 =2 ;
+    let x: bool = true;
+
+    // Type placeholder
+    let x:_ = 1234;
+
+    // println!
     let x = 1;
-    x += 1; // This will cause a compile-time error
-
-    let y = -1;
-
+    println!("x is {}", x);
+    // InLine
+    println!("x is {x}");
+    // Positional Arguments
+    println!("{0} + {0} = {1}", x, x + x);
+    // Debug 
+    println!("DEBUG: x {:?}", x);
+    println!("DEBUG: x {:#?}", x);
 }
