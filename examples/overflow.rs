c258b20eb93e4939e197e8e43a62adf2f0b2fe8b
gogenjack <ivajackdev@gmail.com>
2025-12-21 21:22:49 +0100

    Updated overflow

--- examples/overflow.rs
+++ examples/overflow.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::u32::MAX;
+
 // Overflow doesn't panic when compiling with --release
 fn main() {
     let mut x = u32::MAX;
@@ -7,6 +9,10 @@ fn main() {
     println!("u32 max: {}, x: {}", u32::MAX, x);
 
     // u32:: checked_add - returns None on overflow
-    // u32:: wrapping_add - explicitly allows overflow (wraps around) 
+    let x = u32 :: checked_add(3, 1);
+    println!("checked_add: {:?}", x);
 
+    // u32:: wrapping_add - explicitly allows overflow (wraps around) 
+    let x = u32 :: wrapping_add(u32 :: MAX, 1);
+    println!("wrapping_add: {:?}", x);
 }
