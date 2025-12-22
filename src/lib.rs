8b26816fd6a47ef05f826e4a3683f595494cec67
gogenjack <ivajackdev@gmail.com>
2025-12-22 23:45:30 +0100

    Tuple exercises

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-pub fn mul(x: u32, y: u32) -> u32 {
-    x * y
+pub fn first(t: (bool, u32, char)) -> bool {
+    t.0
 }
 
-pub fn div(x: u32, y: u32) -> u32 {
-    x / y
+pub fn last(t: (bool, u32, char)) -> char {
+    t.2
+}
+
+pub fn swap(t: (u32, u32)) -> (u32, u32) {
+    (t.1, t.0)
 }
