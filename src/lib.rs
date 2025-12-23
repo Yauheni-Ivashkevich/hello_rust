c8ed6355314a31eccc6c7134860a2f1d00bdb71d
gogenjack <ivajackdev@gmail.com>
2025-12-23 23:01:57 +0100

    Rust Arrays in progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-pub fn first(t: (bool, u32, char)) -> bool {
-    t.0
+pub fn zeros() -> [u32; 100] {
+    [0; 100]
 }
 
-pub fn last(t: (bool, u32, char)) -> char {
-    t.2
+pub fn first_3(s: &[u32]) -> &[u32] {
+    &s[..3]
 }
 
-pub fn swap(t: (u32, u32)) -> (u32, u32) {
-    (t.1, t.0)
+pub fn last_3(s: &[u32]) -> &[u32] {
+    let n = s.len();
+    &s[n - 3..]
 }
