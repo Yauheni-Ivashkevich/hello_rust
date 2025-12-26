06c70da397c2f9611c467836a2609279909169c6
gogenjack <ivajackdev@gmail.com>
2025-12-26 18:58:18 +0100

    Enums updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
-pub fn hello() -> String {
-    "Hello Rust".to_string()
-}
+#[derive(Debug, PartialEq)]
 
-pub fn greet(name: &str) -> String {
-    format!("Hello {}", name)
-}
-
-pub fn append(mut s: String) -> String {
-    s += "!";
-    s
+pub enum Color {
+    Red,
+    Green,
+    Blue,
+    Rgba(u8, u8, u8, f32),
 }
